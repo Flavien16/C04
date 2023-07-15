d17ad0aee4520b9a175535435def6245962e0775
Flavien Camoin <fcamoin@z1r5p1.42lyon.fr>
2023-07-15 14:30:38 +0200

    Premier push

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcamoin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 09:48:46 by fcamoin           #+#    #+#             */
+/*   Updated: 2023/07/13 10:02:51 by fcamoin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+#include<unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+/*int	main(void)
+{
+	ft_putstr("Tu es en train de me corriger");
+	return (0);
+}*/
---
+++ ex03/ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcamoin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 11:31:12 by fcamoin           #+#    #+#             */
+/*   Updated: 2023/07/15 07:04:45 by fcamoin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while(str[i] >= 9 && str[i] <= 13 || str[i] == ' ')
+		i++;
+	while(str[i] == '-' || str[i] == '+')
+	{
+		sign *= -1;
+		i++;
+	}
+	while(str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + str[i++] - '0';
+	return (result * sign);
+}
+
+int	main(void)
+{
+	char	c[]= " ---+--+1234ab567";
+	int	result;
+	result = ft_atoi(c);
+	printf("%d", result);
+	return (0);
+}
